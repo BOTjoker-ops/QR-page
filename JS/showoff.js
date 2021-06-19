8673668d0ed770987d207cf94e5c45ab269c4a89
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-06-19 17:10:05 +0200

    allow spaces

--- JS/showoff.js
+++ JS/showoff.js
@@ -6,19 +6,19 @@ const platformIcons = {
     Instagram: '/img/icons/instagram-logo.png',
 }
 
-function getIcon(platform) {
-    if (platform in platformIcons) {
-        return platformIcons[platform]
+function getIcon(label, i) {
+    if (label in platformIcons) {
+        return platformIcons[label]
     } else {
         // needs to return a img some way or another
-        return ''
+        return ""
     }
 }
 
 $(function () {
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     if (hashes.length <= 1) {
-        hashes = ["name=Stijn_van_der_Pouw", "LinkedIn=https://www.linkedin.com/in/stijnvdpouw/", "Facebook=https://www.facebook.com/stijn.vanderpouw/&"];
+        hashes = ["name=Stijn van der Pouw", "LinkedIn=https://www.linkedin.com/in/stijnvdpouw/", "&"];
     }
     
     for (var i = 0; i < hashes.length - 1; i++) {
@@ -29,7 +29,7 @@ $(function () {
             $("#links").append(
                 '<a href="' + pair[1] + '" class="list-group-item list-group-item-action" target="_blank" aria-current="true">' +
                 '<div class="d-flex justify-content-between">' +
-                '<img src=' + getIcon(pair[0]) + ' class="rounded float-left platformIcon">' +
+                '<img id="' + i + '" src=' + getIcon(pair[0], i) + ' class="rounded float-left platformIcon">' +
                 '<h5 class="mb-1 cardTitle">' + decodeURI(pair[0]) + '</h5>' +
                 //     <small>3 days ago</small>
                 //     </div>

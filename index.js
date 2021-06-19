a208b48053261ac6935f32be944b898b9820e505
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-06-19 04:42:05 +0200

    url 0.8

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
+const domainName = '127.0.0.1:5500'
+
 function newRow() {
-    $('#newEntery').remove();
+    $('#newEntry').remove();
     $(".form-inline").append(
-        '<input type="title" class="form-control col injectedForm" placeholder="Title">' +
+        '<input type="label" class="form-control col injectedForm" placeholder="Label">' +
         '<input type="link" class="form-control col injectedForm" placeholder="www">' +
-        '<button id="newEntery" onclick="newRow()" class="btn btn-primary injectedForm">Add link</button>' +
+        '<button id="newEntry" onclick="newRow()" class="btn btn-primary injectedForm">Add link</button>' +
         ' <div class="w-100"></div>'
     )
     $("#initButton").replaceWith(
-        '<button id="submitButton" type="submit" onclick="submitForm()" class="btn btn-primary">Generate QR-code</button>'
+        '<button id="submitButton" type="submit" onclick="makeURL()" class="btn btn-primary">Generate QR-code</button>'
     )
 }
 
-function submitForm() {
-    var values = [];
+function makeURL() {
+
+    var validUrl = 'http://' + domainName + '/?name='
+    var data = [];
     var inputs = $('input')
-    .not(':input[type=button], :input[type=submit], :input[type=reset]');
+        .not(':input[type=button], :input[type=submit], :input[type=reset]');
 
     $(inputs).each(function (e) {
-        var value = $(this).val();
-        values.push(value);
+        data.push($(this).val());
     })
-    console.log(values);
+
+    data.forEach(function (x, i) {
+        if (i != 0) {
+            if (i % 2 == 0) {
+                validUrl = validUrl.concat(x + '=');
+            }
+            if (i % 2 != 0) {
+                validUrl = validUrl.concat(x + '&');
+            }
+        } else {
+            validUrl = validUrl.concat(x);
+        }
+    })
+
+    // Reset form
     $('#cardForm').trigger("reset");
     $('.injectedForm').remove();
     $("#submitButton").replaceWith(
-       '<button id="newRowButton" onclick="newRow()" class="btn btn-primary mb-2">Add link</button>'
-        )
-    // generateQR(values);
+        '<button id="initButton" onclick="newRow()" class="btn btn-primary">Add link</button>'
+    )
+    generateQR(validUrl);
 }
 
+function generateQR(url) {
+    console.log(url);
+}
--- showoff.js
+++ showoff.js
@@ -1,4 +1,4 @@
-// http://127.0.0.1:5500/?Name=Stijn_van_der_Pouw&LinkedIn=https://www.linkedin.com/in/stijnvdpouw/&Facebook=https://www.facebook.com/stijn.vanderpouw/&Instagram=https://www.instagram.com/stijnvdpouw/#
+// http://127.0.0.1:5500/?name=Stijn_van_der_Pouw&LinkedIn=https://www.linkedin.com/in/stijnvdpouw/&Facebook=https://www.facebook.com/stijn.vanderpouw/&Instagram=https://www.instagram.com/stijnvdpouw/&
 
 const platformIcons = {
     LinkedIn: '/img/icons/LinkedIn-icon.png',
@@ -6,41 +6,37 @@ const platformIcons = {
     Instagram: '/img/icons/instagram-logo.png',
 }
 
-function getIcon(platform){
-    if (platform in platformIcons){
+function getIcon(platform) {
+    if (platform in platformIcons) {
         return platformIcons[platform]
-    }
-    else{
+    } else {
         // needs to return a img some way or another
         return ''
     }
-}        
+}
 
 $(function () {
-            var vars = []
-            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-            if (hashes.length <= 1){
-                hashes = ["Name=Stijn_van_der_Pouw", "LinkedIn=https://www.linkedin.com/in/stijnvdpouw/", "Facebook=https://www.facebook.com/stijn.vanderpouw/", "Instagram=https://www.instagram.com/stijnvdpouw/#"];
-            }
-            for (var i = 0; i < hashes.length; i++) {
-                pair = hashes[i].split('=');
-                if(pair[0] == "Name"){
-                    document.getElementById("nameDisplay").innerHTML = pair[1]
-                }
-                else{
-                $("#links").append(
-                    '<a href="' + pair[1] + '" class="list-group-item list-group-item-action" target="_blank" aria-current="true">'+
-                        '<div class="d-flex justify-content-between">'+
-                    '<img src=' + getIcon(pair[0]) + ' class="rounded float-left platformIcon">'+
-                        '<h5 class="mb-1 cardTitle">' + pair[0] + '</h5>' +
-                    //     <small>3 days ago</small>
-                    //     </div>
-                    // <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    // <small>And some small print.</small>
-                    '</a>'
-                )
-                console.log('[server] found: ' + pair[0]);
-                }
-
-            }
-        });
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    if (hashes.length <= 1) {
+        hashes = ["name=Stijn_van_der_Pouw", "LinkedIn=https://www.linkedin.com/in/stijnvdpouw/", "Facebook=https://www.facebook.com/stijn.vanderpouw/", "Instagram=https://www.instagram.com/stijnvdpouw/&"];
+    }
+    for (var i = 0; i < hashes.length - 1; i++) {
+        pair = hashes[i].split('=');
+        if (pair[0] == "name") {
+            document.getElementById("nameDisplay").innerHTML = pair[1]
+        } else {
+            $("#links").append(
+                '<a href="' + pair[1] + '" class="list-group-item list-group-item-action" target="_blank" aria-current="true">' +
+                '<div class="d-flex justify-content-between">' +
+                '<img src=' + getIcon(pair[0]) + ' class="rounded float-left platformIcon">' +
+                '<h5 class="mb-1 cardTitle">' + pair[0] + '</h5>' +
+                //     <small>3 days ago</small>
+                //     </div>
+                // <p class="mb-1">Some placeholder content in a paragraph.</p>
+                // <small>And some small print.</small>
+                '</a>'
+            )
+            console.log('[server] found: ' + pair[0]);
+        }
+    }
+});
